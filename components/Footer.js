ad41a10c018dcfef90d1f2ba6b09680973b07837
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2024-02-15 18:10:06 -0800

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,49 @@
 import Link from "next/link";
 import styles from "../styles/components/Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
-
-
     <div className={styles.background}>
-        <div className={styles.wrapper}>
-
-            <p className={styles.contact}>David Xian  | 604-778-1812</p>
-            <p className={styles.info}>&copy; 2024 PRIDWEN CONSTRUCTION ALL RIGHTS RESERVED</p>
-            
-        </div>
-      
+      <div className={styles.wrapper}>
         <div>
+          <p className={styles.contact}>David Xian | 604-778-1812</p>
+          <p className={styles.contact}>pridwen85@gmail.com</p>
+          <p className={styles.info}>
+            &copy; 2024 PRIDWEN CONSTRUCTION ALL RIGHTS RESERVED
+          </p>
+        </div>
+
+
+      <div>
+        <Image
+          src="/images/main/pacific.png"
+          alt="Description of the image"
+          width={50}
+          height={50}
+          className={styles.iconTC}
+        />
+
+        <Image
+          src="/images/main/warranty.png"
+          alt="Description of the image"
+          width={50}
+          height={50}
+          className={styles.iconTC}
+        />
+
+        <Image
+          src="/images/main/homeowner.png"
+          alt="Description of the image"
+          width={130}
+          height={50}
+          className={styles.iconTC}
+        />
 
         </div>
+      </div>
+
+      <div></div>
     </div>
   );
 }

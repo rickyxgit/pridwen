9bbfcc7b0ecf4fa8e67db3b655a70f9bf3b470ee
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2024-02-17 17:18:18 -0800

    Image tag test

--- components/Slider.js
+++ components/Slider.js
@@ -91,7 +91,7 @@ function MySlider({ images, name }) {
         <StyledReactSlick {...settings}>
           {images.map((image, index) => (
             <Slide key={index}>
-              <img src={image} alt={`Slide ${index + 1}`} className={styles.photo} />
+              <Image src={image} alt={`Slide ${index + 1}`} height={300} width={300} className={styles.photo} />
               <Title>{name}</Title>
             </Slide>
           ))}
